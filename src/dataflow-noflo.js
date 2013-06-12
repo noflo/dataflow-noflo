6b314c7f07287cde8ba24c5e63664d22c718d59b
Forrest Oliphant <forrest@sembiki.com>
2013-06-12 13:41:20 +0300

    basic library plugin populating

--- src/dataflow-noflo.js
+++ src/dataflow-noflo.js
@@ -1,5 +1,38 @@
 ( function(Dataflow) {
  
+  // Make types
+  // Dependencies
+  var Base = Dataflow.prototype.node("base");
+  var NofloBase = Dataflow.prototype.node("noflo-base");
+
+  NofloBase.Model = Base.Model.extend({
+    defaults: function(){
+      var defaults = Base.Model.prototype.defaults.call(this);
+      defaults.type = "noflo-base";
+      return defaults;
+    },
+    initialize: function() {
+      Base.Model.prototype.initialize.call(this);
+    },
+    unload: function(){
+      // Stop any processes that need to be stopped
+    },
+    toJSON: function(){
+      var json = Base.Model.prototype.toJSON.call(this);
+      return json;
+    },
+    inputs:[],
+    outputs:[]
+  });
+
+  NofloBase.View = Base.View.extend({
+    initialize: function() {
+      Base.View.prototype.initialize.call(this);
+    }
+  });
+
+
+  // Make plugin
   var DataflowNoflo = Dataflow.prototype.plugin("noflo");
 
   DataflowNoflo.initialize = function(dataflow){
@@ -12,9 +45,19 @@
       // Plugin: library
       var cl = new noflo.ComponentLoader();
       cl.baseDir = graph.baseDir;
-      cl.listComponents(function(c){
-        console.log( c );
+      cl.listComponents(function(types){
+        for (name in types) {
+          var newType = Dataflow.prototype.node(name);
+          newType.Model = NofloBase.Model.extend();
+          newType.Model.prototype.defaults.name = name;
+          newType.Model.prototype.defaults.nofloType = types[name];
+          newType.View = NofloBase.View.extend();
+        }
       });
+      dataflow.plugins.library.update({
+        exclude: ["base", "base-resizable", "test", "noflo-base"]
+      });
+
 
       // Plugin: source
       var sourceChanged = function(graph) {
@@ -27,12 +70,12 @@
         dataflow.plugins.log.add( "node added: " + JSON.stringify(node) );
         sourceChanged(graph);
       });
-      graph.on("addEdge", function(edge){
-        dataflow.plugins.log.add( "edge added: " + JSON.stringify(edge) );
-        sourceChanged(graph);
-      });
       graph.on("removeNode", function(node){
         dataflow.plugins.log.add( "node removed: " + JSON.stringify(node) );
+        sourceChanged(graph);
+      });
+      graph.on("addEdge", function(edge){
+        dataflow.plugins.log.add( "edge added: " + JSON.stringify(edge) );
         sourceChanged(graph);
       });
       graph.on("removeEdge", function(edge){
